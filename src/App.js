4899e3070a9aae8232b30d3fe861335fa8892a1a
DenEdwards <denzelsedwards@gmail.com>
2020-08-29 23:42:57 -0400

    Added Menu Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/styles.css';
 import Home from "./components/Home";
 import Contact from "./components/Contact";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import {Footer} from "./components/Footer";
 import { BrowserRouter as Router, Route} from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
         <Navbar />
         <Route path="/" exact component={Home}/>
         <Route path="/contact" exact component={Contact}/>
+        <Route path="/menu" exact component={Menu}/>
         <Footer/>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,10 +12,10 @@ function Navbar(){
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto nav-ul">
                         <li className="nav-item active">
-                            <a className="nav-link link" href="#header">Home<span className="sr-only">(current)</span></a>
+                            <Link to="/" className="nav-link link">Home<span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link link" href="#menu">Menu</a>
+                            <Link to="/menu" className="nav-link link" >Menu</Link>
                         </li>
                         <li className="nav-item">
                             <Link to="/contact" className="nav-link link" >Contact</Link>
