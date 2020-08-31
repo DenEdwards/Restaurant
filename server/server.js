f62b94bccdbc3052b00f83da1d23598afa38ca7f
DenEdwards <denzelsedwards@gmail.com>
2020-08-31 13:08:33 -0400

    Changes made for heroku

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const itemRouter = require("./routes/item");
+const path = require('path');
+const buildPath = path.join(__dirname, '..', 'build');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(express.static(buildPath));
 
 app.use("/items", itemRouter);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_ATLAS, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 mongoose.connection.once("open", () => {
